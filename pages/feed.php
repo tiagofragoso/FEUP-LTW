13c61ccfb97d6c833a867d3157bb83d2dc481898
Tiago Fragoso <tiagofragoso@outlook.com>
2018-12-10 09:40:23 +0000

    Changed feed queries

--- pages/feed.php
+++ pages/feed.php
@@ -1,10 +1,12 @@
 <?php 
 	include_once('../includes/session.php');
 	include_once('../templates/common.php');
+	include_once('../database/db_user.php');
 
 	draw_header('SNIPZ - FEED', array('like'));
 	draw_nav();
-	draw_feed();
+	$snippets = empty($_SESSION['user'])? getAllSnippets() : getFeed($_SESSION['user']);
+	draw_feed($snippets);
 	draw_footer();
 
 ?>
