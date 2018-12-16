91c8483be0438c7b6a11d976ab93dc18931e25c7
Tiago Fragoso <tiagofragoso@outlook.com>
2018-12-16 15:47:39 +0000

    Removed unnecessary files

--- actions/post_snippet.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once('../database/db_user.php');
-
-	$snippet = file_get_contents($_FILES['snippet']['tmp_name']);
-	$currDate = (new DateTime())->format('Y-m-d H:i');
-	postSnippet($_POST['title'], $_POST['description'], $snippet, 
-	$_POST['language'], $currDate, $_POST['author']);
-	header("Location: /pages/feed.php");
-?> 
